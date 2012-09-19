f4f5ecdc14bf9aae603d41799a4b8eaa45760569
Geoff Low <glow@mdsol.com>
2012-09-19 20:13:09 +0100

    Added code for Errors

--- checker/view.py
+++ checker/view.py
@@ -14,17 +14,43 @@ jinja_environment = jinja2.Environment(
 
 import model
 
-class ContentCheckRules(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+  # hold the errors
+  ERRORS = []
+  
+  @classmethod
+  def add_error(cls, error):
+    if not error in cls.ERRORS:
+      cls.ERRORS.append(error)
+  
+  @classmethod
+  def get_errors(cls):
+    return cls.ERRORS
+  
+  @classmethod
+  def clear_errors(cls):
+    cls.ERRORS = []
+    
+  def render_jinja(self, name, data):
+    data['logout_url']=users.create_logout_url(self.request.uri)
+    data['errors'] = self.get_errors()
+    self.clear_errors()
+    try:
+      template = jinja_environment.get_template('%s.html' % name)
+    except jinja2.TemplateNotFound:
+      self.redirect("/checker")
+    self.response.out.write(template.render(data))
+        
+class ContentCheckRules(BaseHandler):
   """
   Presents the errors
   """
   def get(self):
     checker = ContentSheetChecker()
     template_values = {'rules' : checker.rule_specifications}
-    template = jinja_environment.get_template('rules.html')
-    self.response.out.write(template.render(template_values))
+    self.render_jinja("rules", template_values)
     
-class ContentCheckError(webapp2.RequestHandler):
+class ContentCheckError(BaseHandler):
   """
   Presents the errors
   """
@@ -33,13 +59,16 @@ class ContentCheckError(webapp2.RequestHandler):
       checklog = model.CheckLog.get_by_id(int(check_id))
     except ValueError:
       # TODO: error
-      self.redirect_to("/checker")
+      self.add_error("Can't convert supplied id %s" % check_id)
+      self.render_jinja("contentchecker", {})
     if not checklog:
       # TODO: error
-      self.redirect_to("/checker")
+      self.add_error("No matching CheckLog with id %s" % check_id)
+      self.render_jinja("contentchecker", {})
     if checklog.findings.count() == 0:
       # TODO: error - no errors exist
-      self.redirect_to("/checker")
+      self.add_error("CheckLog with id %s has no issues" % check_id)
+      self.render_jinja("contentchecker", {})
     else:
       if self.request.get("format"):
         if self.request.get("format") == "csv":
@@ -53,36 +82,35 @@ class ContentCheckError(webapp2.RequestHandler):
         elif self.request.get("format") == "json":
           self.response.headers["Content-type"] = "application/json"
           self.response.out.write(json.dumps([x.as_dict() for x in checklog.findings]))
+        else:
+          self.add_error("Format %s not recognised" % self.request.get('format'))
+          return self.redirect("/checker/errors/%s" % checker.key().id())
       else:
         template_values = {'checklog' : checklog}
-        template = jinja_environment.get_template('outcome.html')
-        self.response.out.write(template.render(template_values))
+        self.render_jinja("outcome", template_values)    
     
-    
-class ContentChecker(webapp2.RequestHandler):
+class ContentChecker(BaseHandler):
 
   def get(self):
-    path = os.path.join(os.path.dirname(__file__), "..", "templates", 'contentcheck.html')
     template_values = {}
-    page = template.render(path, template_values)
-    self.response.out.write(page)
+    self.render_jinja("contentcheck", template_values)    
+
 
   def post(self):
     sheet = self.request.POST["content_sheet"]
     if not hasattr(sheet, "filename"):
       # TODO: Error this
-      print "Redirecting: No file" 
-      self.redirect("/checker")
+      self.add_error("No file")
+      self.render_jinja("contentcheck", {})
     elif sheet.filename.endswith("Template.xlsx"):
       # TODO: Error this
-      print "Redirecting: Filename incorrect"
-      self.redirect("/checker")
+      self.add_error("File name doesn't match pattern")
+      self.redirect("contentcheck", {})
     if users.get_current_user():
       checklog = model.CheckLog(user=users.get_current_user(), sheet=sheet.filename)
       checklog.put()
     else:
       # TODO: Error this
-      print "Redirecting: No current user"
       self.redirect("/checker")
     
     # instantiate the checker
@@ -101,17 +129,12 @@ class ContentChecker(webapp2.RequestHandler):
                                             column=issue.get('column'),
                                             message=issue.get('message'))
         finding.put()
-        print "Logged %s" % issue
       self.redirect("/checker/error/%s" % checklog.key().id())
     else:
       self.redirect("/checker/coffee")
       
-class ContentGenericRenderer(webapp2.RequestHandler):
+class ContentGenericRenderer(BaseHandler):
   
   def get(self, to_render):
-    try:
-      template = jinja_environment.get_template('%s.html' % to_render)
-    except jinja2.TemplateDoesNotExist:
-      self.redirect_to("/checker")
     template_values = {}
-    self.response.out.write(template.render(template_values))
+    self.render_jinja(to_render, template_values)
